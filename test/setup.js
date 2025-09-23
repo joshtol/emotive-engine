cf73ff25dc18eea88d2b0ff802923f2138e0d680
Joshua Tollette <vanityemail@aol.com>
2025-09-23 17:55:03 -0500

    Remove legacy tests - focus on current functionality testing

--- test/setup.js
+++ test/setup.js
@@ -1,69 +1,10 @@
 /**
- * Test setup file for vitest
+ * Test setup file for Emotive Engine
+ * This file will be used for future test development
  */
 
-// Mock canvas context for testing
-HTMLCanvasElement.prototype.getContext = function(contextType) {
-  if (contextType === '2d') {
-    return {
-      clearRect: () => {},
-      fillRect: () => {},
-      strokeRect: () => {},
-      beginPath: () => {},
-      closePath: () => {},
-      moveTo: () => {},
-      lineTo: () => {},
-      arc: () => {},
-      fill: () => {},
-      stroke: () => {},
-      save: () => {},
-      restore: () => {},
-      translate: () => {},
-      rotate: () => {},
-      scale: () => {},
-      fillStyle: '#000000',
-      strokeStyle: '#000000',
-      lineWidth: 1,
-      globalAlpha: 1,
-      font: '10px sans-serif',
-      fillText: () => {},
-      strokeText: () => {},
-      measureText: () => ({ width: 0 }),
-      createRadialGradient: () => ({
-        addColorStop: () => {}
-      }),
-      createLinearGradient: () => ({
-        addColorStop: () => {}
-      })
-    };
-  }
-  return null;
-};
-
-// Mock getBoundingClientRect
-HTMLCanvasElement.prototype.getBoundingClientRect = function() {
-  return {
-    left: 0,
-    top: 0,
-    right: this.width || 300,
-    bottom: this.height || 300,
-    width: this.width || 300,
-    height: this.height || 300,
-    x: 0,
-    y: 0
-  };
-};
-
-// Mock requestAnimationFrame
-global.requestAnimationFrame = (callback) => {
-  return setTimeout(callback, 16);
-};
-
-global.cancelAnimationFrame = (id) => {
-  clearTimeout(id);
-};
-
-// Mock performance.now
-global.performance = {
-  now: () => Date.now()
+// Placeholder for future test setup
+export const testSetup = {
+    version: '4.0.0',
+    description: 'Emotive Engine test setup'
 };
